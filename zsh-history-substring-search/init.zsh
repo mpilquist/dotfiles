71918135acdcd4cdbd2c358dc692526d71e8777e
Michael Pilquist <mpilquist@gmail.com>
2012-09-12 09:57:31 -0400

    Removed prezto dependency

---
+++ zsh-history-substring-search/init.zsh
@@ -0,0 +1,1 @@
+source $ZSH/zsh-history-substring-search/external/zsh-history-substring-search.zsh
---
+++ zsh-syntax-highlighting/init.zsh
@@ -0,0 +1,3 @@
+source $ZSH/zsh-syntax-highlighting/external/zsh-syntax-highlighting.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+
