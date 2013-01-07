48bf51ef159a92c67939f25c96a2421c7f1a0ec3
Michael Pilquist <mpilquist@gmail.com>
2013-01-07 09:30:04 -0500

    Scalaz upgrade, zsh git completion support for new git release, git aliases

--- git/completion.sh
+++ git/completion.sh
@@ -1,9 +1,15 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+completion=/usr/local/share/git-core/contrib/completion/git-completion.zsh
+bash_completion=/usr/local/etc/bash_completion.d/git-completion.bash
 
 if test -f $completion
 then
-  source $completion
+  zstyle ':completion:*:*:git:*' script $bash_completion
+else
+  if test -f $bash_completion
+  then
+    source $bash_completion
+  fi
 fi
