b11a3d268e2c52c0270cd5ab47cec059320b56ea
Michael Pilquist <mpilquist@gmail.com>
2013-01-09 17:40:07 -0500

    Git completion update for newest 1.8.1 brew

--- git/completion.sh
+++ git/completion.sh
@@ -1,7 +1,7 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/share/git-core/contrib/completion/git-completion.zsh
+completion=/usr/local/share/zsh/site-functions/_git
 bash_completion=/usr/local/etc/bash_completion.d/git-completion.bash
 
 if test -f $completion
