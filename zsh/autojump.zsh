ee0cfb2c2a2253c0db3d11de3330665a1d3ddeb1
Michael Pilquist <mpilquist@gmail.com>
2015-01-29 18:52:47 -0500

    Accomodate change in autojump script name

--- zsh/autojump.zsh
+++ zsh/autojump.zsh
@@ -2,4 +2,6 @@ if [ -f `brew --prefix`/etc/autojump ]; then
     . `brew --prefix`/etc/autojump
 elif [ -f `brew --prefix`/etc/autojump.zsh ]; then
     . `brew --prefix`/etc/autojump.zsh
+elif [ -f `brew --prefix`/etc/autojump.sh ]; then
+    . `brew --prefix`/etc/autojump.sh
 fi
